3b14dde5be7ec212537c83529909e8f03a9122c7
Caleb Iuliano <calebiuliano@gmail.com>
2021-06-04 12:34:38 -0400

    fixed profile page

--- client/src/components/Auth/Profile.jsx
+++ client/src/components/Auth/Profile.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import getUserInfo from './helpers/getUserInfo.js';
 import firebase from 'firebase';
 import 'firebase/auth';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 import no from './assets/Boton-mal.svg';
 import yes from './assets/dwcheckyes.svg';
 
@@ -22,32 +29,56 @@ class Profile extends React.Component {
 
   render () {
     return (
-      <div className="userInfoContainer">
+      <div>
         <div className="userInfo">
-
-            <h3 className="bold underline">Name:</h3> {this.auth.currentUser.displayName}<br/>
-            <h3 className="bold underline">Email:</h3> {this.auth.currentUser.email}<br/>
-            {this.state.user
-            ?<div>
-                <h3 className="bold underline">Phone:</h3> {this.state.user.phone}<br/>
-                <h3 className="bold underline">Facebook:</h3> {this.state.user.facebook}<br/>
-                <h3 className="bold underline">Youtube:</h3> {this.state.user.youtube}<br/>
-                <h3 className="bold underline">Twitter:</h3> {this.state.user.twitter}<br/>
-                <h3 className="bold underline">Instagram:</h3> {this.state.user.instagram}<br/>
-                <h3 className="bold underline">City:</h3> {this.state.user.city}<br/>
-                <h3 className="bold underline">State:</h3> {this.state.user.state}<br/>
-                <h3 className="bold underline">Country:</h3> {this.state.user.country}<br/>
-                <h3 className="bold underline">Hosts Athletes:</h3><br/>
-                {this.state.user.host
+          <div className="profilePic">
+            <h3>{this.auth.currentUser.displayName}</h3>
+            <img className="profilePic" src={this.auth.currentUser.photoURL}></img>
+          </div>
+          {!this.state.user
+          ? null
+          : <div className="contentContainer">
+              <div className="contents">
+                <span><h3 className="underline bold">City:</h3> {this.state.user.city}</span><br></br>
+                <span><h3 className="underline bold">State:</h3> {this.state.user.state}</span><br></br>
+                <span><h3 className="underline bold">Country:</h3> {this.state.user.country}</span><br/>
+                <span><h3 className="underline bold">Email:</h3> {this.state.user.email}</span><br></br>
+                <span><h3 className="underline bold">Phone:</h3> {this.state.user.phone}</span><br/>
+                <span><h3 className="underline bold">Hosts Athletes:</h3> <br/>{this.state.user.host
                   ? <img src={yes} style={{width: '5vw', height:'auto'}}/>
-                  : <img src={no} style={{width: '5vw', height:'auto'}}/>}
+                  : <img src={no} style={{width: '5vw', height:'auto'}}/>}</span>
+              </div>
+            </div>}
+          </div>
+          {this.state.user ?
+          <div className="socialMedia">
+            <a href={this.state.user.youtube || null}
+              className="youtube social">
+              <FontAwesomeIcon icon={faYoutube} size="1x" />
+            </a>
+            <a href={this.state.user.facebook || null}
+              className="facebook social">
+              <FontAwesomeIcon icon={faFacebook} size="1x" />
+            </a>
+            <a href={this.state.user.twitter || null} className="twitter social">
+              <FontAwesomeIcon icon={faTwitter} size="1x" />
+            </a>
+            <a href={this.state.user.instagram || null}
+              className="instagram social">
+              <FontAwesomeIcon icon={faInstagram} size="1x" />
+            </a><br/>
+          </div> : null}
+        {this.auth.currentUser && this.state.user && this.state.user.email === this.auth.currentUser.email
+          ? <div id="deleteLi">
+              <div></div>
+              <span
+              className="seeMore deleteLi"
+              onClick={()=> this.state.showEditModal(this.state.user)}
+              >Edit Info?</span>
             </div>
-              : null}
-        </div>
-        <div className="profilePic">
-          <img className="profilePic" src={this.auth.currentUser.photoURL} />
-        </div>
-      </div>
+            : null}
+    </div>
+
     )
   }
 }
