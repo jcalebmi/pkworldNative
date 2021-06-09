5abce41a7694bdbc718ee85f56581ffc1ed4caaf
Caleb Iuliano <calebiuliano@gmail.com>
2021-06-09 11:44:43 -0400

    Fixed events bug

--- client/src/components/Events/Event.jsx
+++ client/src/components/Events/Event.jsx
@@ -44,7 +44,7 @@ class Event extends React.Component {
                 </div>
                 <br/>
                 <span className="bold seeMore" onClick={this.showMore}>Close</span><br/>
-                {this.auth.currentUser && this.props.event.email === this.auth.providerData[0].email
+                {this.auth.currentUser && this.props.event.email === this.auth.currentUser.providerData[0].email
               ? <div id="deleteLi">
                   <div></div>
                   <span
