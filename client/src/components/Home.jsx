71d817de893e1eb4ccd7d805eb9d844d8e5b62aa
Caleb Iuliano <calebiuliano@gmail.com>
2021-06-15 00:16:02 -0400

    rellax updates

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import stride from '../assets/stride.png';
-import trio from '../assets/trio.png';
+import trio from '../assets/trio2.png';
 import air from '../assets/Air.png';
-import imax from '../assets/imax.png';
+import imax from '../assets/imax2.png';
 import gareth from '../assets/gareth.png';
 import garethLay from '../assets/garethlay.png';
+import eric from '../assets/eric.png';
+import tampaHalf from '../assets/half.png';
 import Rellax from 'rellax';
 
 class Home extends React.Component {
@@ -61,7 +63,7 @@ class Home extends React.Component {
     })
     new Rellax('.garethLay', {
       breakpoints: [200, 480, 576, 768, 1201],
-      speed: 9,
+      speed: 4,
       center: false,
       wrapper: '.body',
       round: false,
@@ -88,7 +90,25 @@ class Home extends React.Component {
     })
     new Rellax('.events', {
       breakpoints: [200, 480, 576, 768, 1201],
-      speed: -8,
+      speed: -9,
+      center: false,
+      wrapper: '.body',
+      round: false,
+      vertical: true,
+      horizontal: false
+    })
+    new Rellax('.eric', {
+      breakpoints: [200, 480, 576, 768, 1201],
+      speed: -4,
+      center: false,
+      wrapper: '.body',
+      round: false,
+      vertical: true,
+      horizontal: false
+    })
+    new Rellax('.half', {
+      breakpoints: [200, 480, 576, 768, 1201],
+      speed: 1,
       center: false,
       wrapper: '.body',
       round: false,
@@ -108,6 +128,8 @@ class Home extends React.Component {
         <img className="rellax air" src={air} />
         <img className="rellax imax" src={imax} />
         <img className="rellax garethLay" src={garethLay} />
+        <img className="rellax eric" src={eric} />
+        <img className="rellax half" src={tampaHalf} />
       </div>
     )
   }
